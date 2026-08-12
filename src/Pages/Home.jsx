374ffd8f0369021b10a833f21d4d0889b659b1de
sokheng429-wq <sokheng429@gmail.com>
2026-08-12 08:58:39 +0700

    update homepage 5

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import { useLanguage } from '../context/LanguageContext'
 import homeHero from '../assets/Store.png'
 import homeHero2 from '../assets/Home.png'
-import inside from '../assets/Inside.png'
+import inside from '../assets/inside.png'
 import './Home.css'
 
 const VALUES = [
